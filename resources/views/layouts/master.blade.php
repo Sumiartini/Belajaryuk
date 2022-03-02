f3436b0fd3619c9e7856062cc00417eea4ab556c
Sumiartini <Sumiartinisrirahayu@gmail.com>
2022-03-02 15:58:12 +0700

    [Lrvl-templating]fix gambar logo sidebar

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -14,7 +14,7 @@
  <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
     <a href="" class="brand-link">
-      <img src="{{ asset('adminlte/dist/img/chicken.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+      <img src="{{ asset('adminlte/dist/img/chicken-logo.jpg') }}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
       <span class="brand-text font-weight-light">Warung Rindu</span>
     </a>
     @include('layouts.sidebar')
