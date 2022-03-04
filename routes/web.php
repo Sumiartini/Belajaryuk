a9c4df895300c3924131f5eb9b738aa4341628ff
Sumiartini <Sumiartinisrirahayu@gmail.com>
2022-03-04 13:33:30 +0700

    [Lrvl-templating]membuat halaman beranda

--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('beranda');
 })->name('task');
 
 Route::post('/',function(Request $request) {
