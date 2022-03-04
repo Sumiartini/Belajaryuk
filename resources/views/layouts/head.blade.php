aac1af272c0beadac5a5e1c744961096140cc6d9
Sumiartini <Sumiartinisrirahayu@gmail.com>
2022-03-04 14:00:44 +0700

    [Lrvl-templating]ubah icon tab

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -3,6 +3,8 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>{{ config('app.name', 'Laravel') }}</title>
 
+  <!-- For favicon png -->
+  <link rel="shortcut icon" type="image/icon" href="assets/images/chicken/chicken-logo.jpg"/>
   <!-- Google Font: Source Sans Pro -->
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
   <!-- Font Awesome -->
